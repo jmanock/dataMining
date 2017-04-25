a480b3353baa919e1ea881e059820fb37cd8029e
Jon Manock <jonmanock@gmail.com>
2017-04-25 10:56:05 -0400

    setting it up

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
-var fs = require('fs');
-var readline = require('readline');
-var stream = require('stream');
-var files = ['../Desktop/orange2:17.txt', '../Desktop/seminal2:17.txt'];
-var instream = fs.createReadStream(files);
-var outstream = new stream;
-var rl = readline.createInterface(instream, outstream);
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-rl.on('line', function(line){
-  /*
-    ~ Should be able to edit data
-    ~ Only get back info that is needed
-  */
-  if(line.includes('JEB')){
-    something(line);
-  }
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
-rl.on('close', function(){
-  console.log('GameOver!!!');
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
-function something(x){
-  // This has an email address in the line
-  /*
-    ~ Maybe throw these in a spread sheet?
-  */
-  console.log(x);
-}
+
+module.exports = app;
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-  console.log('HelloFreindz');
+  $('#search').on('keyup', function(e){
+    if(e.keyCode === 13){
+      var search = $(this).val();
+      var split = search.split(' ');
+      var firstName = split[0];
+      var lastName = split[1];
+
+      var params = {
+        firstName:firstName,
+        lastName:lastName
+      };
+      $.get('/searching', params, function(data){
+        
+      });
+    }
+  })
 });
