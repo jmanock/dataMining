868b432899a87d72343df20665163851dd4ad91d
Jon Manock <jonmanock@gmail.com>
2017-04-25 11:34:07 -0400

    this works, need to add more files, and work on the output

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var readline = require('readline');
 var stream = require('stream');
-//var instream = fs.createReadStream('');
-var outstream = new stream;
-//var rl = readline.createInterface(instream, outstream);
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -30,26 +27,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.get('/search', function(req, res){
+  var instream = fs.createReadStream('public/files/orange2:17.txt');
+  var outstream = new stream;
+  var rl = readline.createInterface(instream, outstream);
   var firstName = req.query.firstName;
   var lastName = req.query.lastName;
-  console.log(firstName, lastName);
-});
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-//
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
+  rl.on('line', function(line){
+    if(line.includes(firstName) && line.includes(lastName)){
+      console.log(line);
+    }
+  });
+  rl.on('close', function(){
+    console.log('Im Done');
+  });
+  function search(line){
+    console.log(line);
+  }
+});// End `/search`
 module.exports = app;
