b0f2cf14a47ef59c35ceeb6f83f2c778dd8aee35
Jon Manock <jonmanock@gmail.com>
2017-04-26 12:33:11 -0400

    eff, not sure why some records wont work

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ app.get('/search', function(req, res){
   var lastName = req.query.lastName;
   console.log(firstName, lastName);
   rl.on('line', function(line){
-    if(line.includes(113282526)){
-      console.log(line);
+    if(line.includes(firstName)&&line.includes(lastName)){
+      Search(line);
     }
   }).on('close', function(){
     console.log('GameOver!');
@@ -62,8 +62,7 @@ app.get('/search', function(req, res){
     var zip = results[10];
     var sex = results[11];
     var dob = results[13];
-
-    console.log(county,voterId,firstName,lastName,address,city,zip,dob);
+    console.log(results);
   }// End `Search Function`
 });// End `/search`
 module.exports = app;
